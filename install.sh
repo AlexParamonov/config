2559439fe3f4a919b3b315ce92108701c635202e
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2012-05-12 20:59:20 +0300

    Add install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for config in `ls -1A | grep -v -E "(install.sh|.rvm|.local|.ssh|.git)$"`
+do
+  echo "installing $config"
+  rm "$HOME/$config"
+  ln -s "`pwd`/$config" "$HOME/$config"
+done
+
+echo "Done."
+
+echo
+echo "Updating fonts..."
+echo `fc-cache -fv | grep "fc-cache"`
+
+echo
+echo "================================================="
+echo "  WARNING! Install .ssh, .rvm, .local manually."
+echo "================================================="

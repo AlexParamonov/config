a91ca2af80a2c5e3a176441ca9ebc75b47b60736
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2012-12-25 22:06:34 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for config in `ls -1A | grep -v -E "(install.sh|.rvm|.local|.ssh|.git|.gitignore|.gitmodules)$"`
+for config in `ls -1A | grep -E "^\." | grep -v -E "(\.rvm|\.local|\.ssh|\.git|\.gitignore|\.gitmodules)$"`
 do
   echo "installing $config"
-  rm -r "$HOME/$config"
+  rm -r "$HOME/$config" 2> /dev/null
   ln -s "`pwd`/$config" "$HOME/$config"
 done
 
@@ -12,8 +12,6 @@ echo "Done."
 # echo
 # echo "Updating fonts..."
 # echo `fc-cache -fv | grep "fc-cache"`
-
-eval `dircolors ~/.dircolors`
 
 echo
 echo "================================================="

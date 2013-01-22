aec3b619c223ddb1c0387eee1cec4dbad60bdf92
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2013-01-22 21:08:15 +0200

    [BASH] Add vagrant to PATH

--- .bash/env.sh
+++ .bash/env.sh
@@ -3,7 +3,8 @@ export EDITOR=vim
 export VISUAL=vim
 
 export PATH=$PATH:/usr/lib/jvm/java-6-sun/bin
-export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+export PATH=$PATH:$HOME/.rvm/bin
+export PATH=$PATH:/opt/vagrant/bin
 
 export JDK_HOME=/usr/lib/jvm/java-6-sun
 export RUBYMINE_JDK=/usr/lib/jvm/java-6-sun

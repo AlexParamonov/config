f90da9b964d22196eb9635b3d46ff46ef65da35c
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2013-02-11 17:32:20 +0200

    [BASH] Add ack-grep => ack allias

--- link_install/.bash/aliases.sh
+++ link_install/.bash/aliases.sh
@@ -15,6 +15,9 @@ alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
 
+alias ack='ack-grep'
+alias notes="ack 'TODO|FIXME|XXX|HACK' --ignore-dir=tmp --ignore-dir=log"
+
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'

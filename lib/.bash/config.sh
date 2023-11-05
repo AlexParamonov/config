dcef5ad29cc31ef23247917ce015fcd20c1df280
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2023-11-05 15:28:03 +0100

    [BASH] possible fix for "flying lines"

--- lib/.bash/config.sh
+++ lib/.bash/config.sh
@@ -1,6 +1,6 @@
 # check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
-shopt -s checkwinsize
+# shopt -s checkwinsize
 
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"

6c54befa8f341efc650ccc37d69eeb6e8d520824
Alexander Paramonov <352030+AlexParamonov@users.noreply.github.com>
2026-02-26 19:46:33 +0100

    [BASH] new asdf

--- lib/.bash/asdf.sh
+++ lib/.bash/asdf.sh
@@ -1,3 +1,5 @@
 [[ -s "$HOME/.asdf/asdf.sh" ]] && source "$HOME/.asdf/asdf.sh"
 [[ -s "$HOME/.asdf/completions/asdf.bash" ]] && source "$HOME/.asdf/completions/asdf.bash"
 [[ -s "/usr/local/opt/asdf/libexec/asdf.sh" ]] && source "/usr/local/opt/asdf/libexec/asdf.sh"
+
+export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
